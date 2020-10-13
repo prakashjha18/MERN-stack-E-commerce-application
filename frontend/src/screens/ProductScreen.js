25912876af6dcaa179c44d4c909b584c4368c2af
Prakash <prkjha25@gmail.com>
2020-10-13 20:22:09 +0530

    serving products- backend routes

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,7 +5,7 @@ import Rating from '../components/Rating'
 import products from '../products'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id == match.params.id)
+  const product = products.find((p) => p._id === match.params.id)
 
   return (
     <>
