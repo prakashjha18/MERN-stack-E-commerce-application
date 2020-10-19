8e32b529fe0a0b31c3ca18db1122cee0bc8a56cd
Prakash <prkjha25@gmail.com>
2020-10-20 02:26:49 +0530

    order controller and route

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express'
+const router = express.Router()
+import { addOrderItems } from '../controllers/orderController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+router.route('/').post(protect, addOrderItems)
+export default router
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import connectDB from './config/db.js'
 import { notFound,errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 connectDB()
 
@@ -20,6 +21,7 @@ app.get('/',(req,res) => {
 
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
+app.use('/api/orders',orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
