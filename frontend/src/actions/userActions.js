0ca8adf722d02ce154f2f1e96eedd24248d54db4
Prakash <prkjha25@gmail.com>
2020-10-21 02:37:14 +0530

    use details and order reset

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -52,8 +54,11 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
-  dispatch({ type:USER_LOGOUT})
+  dispatch({ type: USER_LOGOUT })
+  dispatch({ type: USER_DETAILS_RESET })
+  dispatch({ type: ORDER_LIST_MY_RESET })
 }
+
 
 export const register = (name, email, password) => async (dispatch) => {
   try {
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,7 @@
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -60,6 +61,8 @@ export const userDetailsReducer = (state = {user:{}}, action) => {
       }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
+    case USER_DETAILS_RESET:
+      return { use:{} }
     default:
       return state
   }
