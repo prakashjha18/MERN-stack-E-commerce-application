8cb38c06e33fa338cfc5895c41e79bebb290a018
Prakash <prkjha25@gmail.com>
2020-10-21 23:35:36 +0530

    User edit screen and get user functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
+import UserEditScreen from './screens/UserEditScreen'
 
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
         <Route path='/admin/userlist' component={UserListScreen}/>
+        <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
         <Route path='/order/:id' component={OrderScreen}/>
         <Route path='/placeorder' component={PlaceOrderScreen}/>
         <Route path='/payment' component={PaymentScreen}/>
