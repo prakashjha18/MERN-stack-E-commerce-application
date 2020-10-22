2560b0ad7a63927cf8f92ff1af4aafc990198622
Prakash <prkjha25@gmail.com>
2020-10-22 21:16:04 +0530

    Morgan and create review endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,14 @@ import {
   getProductById,
   deleteProduct,
   createProduct,
-  updateProduct
+  updateProduct,
+  createProductReview,
 } from '../controllers/productController.js'
 
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect,admin,createProduct)
+router.route('/:id/reviews').post(protect,createProductReview)
 
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect,admin,updateProduct)
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import express from  'express'
 import dotenv from 'dotenv'
-
+import morgan from 'morgan'
 dotenv.config()
 
 import connectDB from './config/db.js'
@@ -16,6 +16,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
 
 app.get('/',(req,res) => {
     res.send('API is running...');
