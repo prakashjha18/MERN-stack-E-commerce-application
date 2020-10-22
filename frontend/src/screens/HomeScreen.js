3a1a808eb2744ccdee3914d09dbecc679cf64f9a
Prakash <prkjha25@gmail.com>
2020-10-23 00:46:47 +0530

    Custom page titles and meta

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
@@ -7,6 +8,7 @@ import Loader from '../components/Loader'
 import ProductCarousel from '../components/ProductCarousel'
 import { listProducts } from '../actions/productActions'
 import Paginate from '../components/Paginate'
+import Meta from '../components/Meta'
 
 const HomeScreen = ({match}) => {
   const keyword = match.params.keyword
@@ -24,7 +26,14 @@ const HomeScreen = ({match}) => {
 
   return (
     <>
-      {!keyword && <ProductCarousel/>}
+    <Meta/>
+    {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader/>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,6 +7,7 @@ import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails,createProductReview } from '../actions/productActions'
 import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
+import Meta from '../components/Meta'
 
 const ProductScreen = ({ history,match }) => {
   const [qty, setQty] = useState(1)
@@ -63,6 +64,7 @@ const ProductScreen = ({ history,match }) => {
         <Message variant='danger'>{error}</Message>
       ) : (
         <>
+        <Meta title={product.name} />
         <Row>
           <Col md={6}>
             <Image src={product.image} alt={product.name} fluid />
