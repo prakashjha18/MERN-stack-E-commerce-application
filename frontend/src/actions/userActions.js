b545b63f3085333667395d6924e699153df088f3
Prakash <prkjha25@gmail.com>
2020-10-23 20:49:29 +0530

    update profile bug fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -172,6 +172,10 @@ export const updateUserProfile = (user) => async (dispatch,getState) => {
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
     })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
     localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
     dispatch({
